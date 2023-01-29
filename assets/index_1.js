3a22f2c59d482a432483772feeb105b1579c5936
Arpan Kumar <arpankumar007@outlook.com>
2023-01-29 10:49:44 +0530

    Add files via upload

---
+++ assets/index_1.js
@@ -0,0 +1,23 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+    document.getElementsByClassName("fp-tooltip").style.visibility = "hidden";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+    loader.style.display = "none";
+});
+
+new fullpage("#fullpage", {
+    autoScrolling: true,
+    navigation: true,
+    navigationTooltips: ['Home', "Elements", ""],
+    showActiveTooltip: true,
+    slidesNavigation: true,
+    slidesNavPosition: "middle",
+
+});
